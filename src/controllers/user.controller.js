d35f059b359b071394aead85520fff262717c15d
rafaelsoeiro <103294767+rafaelsoeiro@users.noreply.github.com>
2022-10-30 17:14:53 -0300

    feat: add try/catch in controller and middleware global

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,68 +1,101 @@
 const userService = require("../services/user.service");
 
 const create = async (req, res) => {
-    const { name, username, email, password, avatar, background } = req.body;
+    try {
+        const { name, username, email, password, avatar, background } = req.body;
 
-    if (!name || !username || !email || !password || !avatar || !background) {
-        res.status(400).send({ message: "submit all fields for registration" });
+        if (
+            !name ||
+            !username ||
+            !email ||
+            !password ||
+            !avatar ||
+            !background
+        ) {
+            res.status(400).send({
+                message: "submit all fields for registration",
+            });
+        }
+
+        const user = await userService.createService(req.body);
+
+        if (!user) {
+            return res.status(400).send({ message: "Error creating user" });
+        }
+
+        res.status(201).json({
+            message: "User created sucessfully",
+            user: {
+                id: user._id,
+                name,
+                username,
+                email,
+                avatar,
+                background,
+            },
+        });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
-
-    const user = await userService.createService(req.body);
-
-    if (!user) {
-        return res.status(400).send({ message: "Error creating user" });
-    }
-
-    res.status(201).json({
-        message: "User created sucessfully",
-        user: {
-            id: user._id,
-            name,
-            username,
-            email,
-            avatar,
-            background,
-        },
-    });
 };
 
 const findAll = async (req, res) => {
-    const users = await userService.findAllService();
+    try {
+        const users = await userService.findAllService();
 
-    if (users.length === 0) {
-        return res
-            .status(400)
-            .send({ message: "Thare are no registered users" });
+        if (users.length === 0) {
+            return res
+                .status(400)
+                .send({ message: "Thare are no registered users" });
+        }
+        res.send(users);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
-    res.send(users);
 };
 
 const findById = async (req, res) => {
-    const id = req.params.id;
+    try {
+        const id = req.params.id;
 
-    const user = req.user;
+        const user = req.user;
 
-    res.send(user);
+        res.send(user);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
 };
 
 const update = async (req, res) => {
-    const { name, username, email, password, avatar, background } = req.body;
-    if (!name && !username && !email && !password && !avatar && !background) {
-        res.status(400).send({ message: "submit at least one for update" });
+    try {
+        const { name, username, email, password, avatar, background } =
+            req.body;
+        if (
+            !name &&
+            !username &&
+            !email &&
+            !password &&
+            !avatar &&
+            !background
+        ) {
+            res.status(400).send({ message: "submit at least one for update" });
+        }
+
+        const { id, user } = req;
+
+        await userService.updateService(
+            id,
+            name,
+            username,
+            email,
+            password,
+            avatar,
+            background
+        );
+        res.send({ message: "User succesfully updated" });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
-
-    const { id, user } = req;
-
-    await userService.updateService(
-        id,
-        name,
-        username,
-        email,
-        password,
-        avatar,
-        background
-    );
-    res.send({ message: "User succesfully updated" });
 };
 
 module.exports = {
